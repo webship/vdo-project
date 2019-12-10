3fd055a09c340d3bfc2e9acf8f626f7559c865ef
Rajab Natshah <rajabn@gmail.com>
2019-12-10 15:11:11 +0200

    Added Paragon distribution to profiles workspace

---
+++ profiles/cmd-vdo-build-profiles-paragon.sh
@@ -0,0 +1,19 @@
+#!/bin/usr/env bash
+
+echo "*---------------------------------------------------*";
+echo "| Build Paragon                                     |";
+echo "*---------------------------------------------------*";
+echo "| https://www.drupal.org/project/paragon            |";
+echo "*---------------------------------------------------*";
+
+# Bootstrap VDO.
+. ${vdo_scripts}/bootstrap.sh ;
+
+# Load workspace settings and extra lists.
+eval $(parse_yaml ${vdo_config}/workspace.profiles.settings.yml);
+
+composer create-project elevatedthird/paragon-base paragon --no-interaction --stability=alpha;
+
+full_database_name="${database_prefix}paragon";
+mysql -u${database_username} -p${database_password} -e "DROP DATABASE IF EXISTS ${full_database_name};"
+mysql -u${database_username} -p${database_password} -e "CREATE DATABASE ${full_database_name};"
