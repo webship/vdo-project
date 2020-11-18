e5062da0692a0c29d0904158f46d54540d45388b
Rajab Natshah <rajabn@gmail.com>
2020-11-18 19:53:35 +0200

    Add a new VDO Tool bash file to change git config filemode to false in all workspace config directory

---
+++ sandboxes/cmd-vdo-tool-git-change-filemode-to-false.sh
@@ -0,0 +1,15 @@
+#!/bin/usr/env bash
+
+# Bootstrap VDO.
+. ${vdo_scripts}/bootstrap.sh ;
+
+# Load workspace settings and extra lists.
+eval $(parse_yaml ${vdo_config}/workspace.sandboxes.settings.yml);
+
+for dir_name in *; do
+    if [[ -d "$dir_name" && ! -L "$dir_name" ]]; then
+        if [ -f "${vdo_root}/${doc_name}/${dir_name}/.git/config" ]; then
+          sed -i -e 's/filemode = true/filemode = false/g' ${vdo_root}/${doc_name}/${dir_name}/.git/config
+        fi
+    fi
+done
