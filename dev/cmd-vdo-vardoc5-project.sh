ca0a6dc5111efb5adfc540b7b8d98cf6c21fc6c8
Rajab Natshah <rajabn@gmail.com>
2021-05-02 11:29:51 +0300

    Add DEV builder for Vardoc 5.0.x #29

---
+++ dev/cmd-vdo-vardoc5-project.sh
@@ -0,0 +1,72 @@
+#!/bin/usr/env bash
+
+# Bootstrap VDO.
+. ${vdo_scripts}/bootstrap.sh ;
+
+# Load workspace settings and extra lists.
+eval $(parse_yaml ${vdo_config}/workspace.dev.settings.yml);
+
+# Change with the version of Varbase 5.0.x-dev
+site_version="5.0.x-dev";
+# Change with the version of Varbase
+site_version_code="50DEV";
+
+
+# Change to true if you want to install varbase.
+install_site=false;
+
+base_url="${web_url}/${project_name}";
+
+# GET the project name argument.
+if [ "$1" != "" ]; then
+    project_name=$1;
+else
+  echo "Please add the name of your project.";
+  exit 1;
+fi
+
+# GET install argument to install;
+if [ "$2" != "" ]; then
+  if [ "$2" == "install" ]; then
+    install_site=true;
+  fi
+fi
+
+# Change directory to the workspace for this full operation.
+cd ${vdo_root}/${doc_name};
+
+if [ -d "${project_name}" ]; then
+  sudo rm -rf ${project_name} -vvv
+fi
+
+full_database_name="${database_prefix}${project_name}";
+mysql -u${database_username} -p${database_password} -e "DROP DATABASE IF EXISTS ${full_database_name};" -vvv
+mysql -u${database_username} -p${database_password} -e "CREATE DATABASE ${full_database_name};" -vvv
+
+composer create-project vardot/vardoc-project:${site_version} ${project_name} --stability dev --no-interaction -vvv
+
+sudo chmod 775 -R ${project_name}
+sudo chown www-data:${user_name} -R ${project_name}
+
+echo "${doc_name} ${project_name} is ready to install!!!!";
+echo "Go to ${base_url}";
+
+if $install_site ; then
+  # Change directory to the docroot.
+  cd ${vdo_root}/${doc_name}/${project_name}/docroot;
+
+  # Install Varbase with Drush.
+  drush site-install vardoc --yes \
+  --site-name="${doc_name} ${project_name}" \
+  --account-name="${account_name}" \
+  --account-pass="${account_pass}" \
+  --account-mail="${account_mail}" \
+  --db-url="mysql://${database_username}:${database_password}@${database_host}/${full_database_name}" varbase_development_tools.varbase_development=1 -vvv;
+
+  # Send a notification.
+  echo "${doc_name} ${project_name} has been installed!!!!";
+  echo  "Go to ${base_url}";
+  cd ${vdo_root}/${doc_name};
+  sudo chmod 775 -R ${project_name};
+  sudo chown www-data:${user_name} -R ${project_name};
+fi
