c9fcd00acd55aff6b33a4a513b2ca7359b4286d7
Rajab Natshah <rajabn@gmail.com>
2021-05-10 10:05:57 +0300

    Add the database connection that Drupal may use on install #30

--- dev/cmd-vdo-varbase9-project.sh
+++ dev/cmd-vdo-varbase9-project.sh
@@ -43,10 +43,53 @@ full_database_name="${database_prefix}${project_name}";
 mysql -u${database_username} -p${database_password} -e "DROP DATABASE IF EXISTS ${full_database_name};" -vvv
 mysql -u${database_username} -p${database_password} -e "CREATE DATABASE ${full_database_name};" -vvv
 
-composer create-project vardot/varbase-project:${site_version} ${project_name} --stability dev --no-interaction -vvv
+composer create-project vardot/varbase-project:${site_version} ${project_name} --stability dev --no-interaction -vvv ;
 
-sudo chmod 775 -R ${project_name}
-sudo chown www-data:${user_name} -R ${project_name}
+cp ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/default.settings.php ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+echo "\$databases['default']['default'] = array (
+  'database' => '${full_database_name}',
+  'username' => '${database_username}',
+  'password' => '${database_password}',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);" >> ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+
+mkdir ${vdo_root}/${doc_name}/${project_name}/config ;
+mkdir ${vdo_root}/${doc_name}/${project_name}/config/sync ;
+echo "# Deny all requests from Apache 2.4+.
+<IfModule mod_authz_core.c>
+  Require all denied
+</IfModule>
+
+# Deny all requests from Apache 2.0-2.2.
+<IfModule !mod_authz_core.c>
+  Deny from all
+</IfModule>
+
+# Turn off all options we don't need.
+Options -Indexes -ExecCGI -Includes -MultiViews
+
+# Set the catch-all handler to prevent scripts from being executed.
+SetHandler Drupal_Security_Do_Not_Remove_See_SA_2006_006
+<Files *>
+  # Override the handler again if we're run later in the evaluation list.
+  SetHandler Drupal_Security_Do_Not_Remove_See_SA_2013_003
+</Files>
+
+# If we know how to do it safely, disable the PHP engine entirely.
+<IfModule mod_php7.c>
+  php_flag engine off
+</IfModule>" >> ${vdo_root}/${doc_name}/${project_name}/config/sync/.htaccess;
+echo "\$settings['config_sync_directory'] = '../config/sync';" >> ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+
+vdo_build_time=$( date '+%Y-%m-%d %H-%M-%S' );
+echo "// VDO Built time: ${vdo_build_time}" >> ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+
+sudo chmod 775 -R ${vdo_root}/${doc_name}/${project_name}
+sudo chown www-data:${user_name} -R ${vdo_root}/${doc_name}/${project_name}
 
 echo "${doc_name} ${project_name} is ready to install!!!!";
 echo "Go to ${base_url}";
@@ -56,23 +99,9 @@ if $install_site ; then
   cd ${vdo_root}/${doc_name}/${project_name}/docroot;
 
   # Install Varbase with Drush.
-  drush site-install varbase --yes \
-  --site-name="${doc_name} ${project_name}" \
-  --account-name="${account_name}" \
-  --account-pass="${account_pass}" \
-  --account-mail="${account_mail}" \
-  --db-url="mysql://${database_username}:${database_password}@${database_host}/${full_database_name}" \
-  varbase_multilingual_configuration.enable_multilingual=true \
-  varbase_extra_components.vmi=true \
-  varbase_extra_components.varbase_heroslider_media=true \
-  varbase_extra_components.varbase_carousels=true \
-  varbase_extra_components.varbase_search=true \
-  varbase_extra_components.varbase_blog=true \
-  varbase_extra_components.varbase_auth=true \
-  varbase_development_tools.varbase_development=true -vvv;
+  drush site-install varbase --yes  --site-name="${doc_name} ${project_name}"  --account-name="${account_name}"  --account-pass="${account_pass}"  --account-mail="${account_mail}"  --db-url="mysql://${database_username}:${database_password}@${database_host}/${full_database_name}" varbase_multilingual_configuration.enable_multilingual=true varbase_extra_components.vmi=true varbase_extra_components.varbase_heroslider_media=true varbase_extra_components.varbase_carousels=true varbase_extra_components.varbase_search=true varbase_extra_components.varbase_blog=true varbase_extra_components.varbase_auth=true varbase_development_tools.varbase_development=true -vvv;
 
   drush pm-enable varbase_styleguide --yes ;
-  drush pm-enable vbp_text_and_image --yes ;
   drush pm-enable varbase_media_instagram --yes ;
   drush pm-enable varbase_media_twitter --yes ;
   drush pm-enable social_auth_google --yes ;
--- dev/cmd-vdo-vardoc1-project.sh
+++ dev/cmd-vdo-vardoc1-project.sh
@@ -43,10 +43,54 @@ full_database_name="${database_prefix}${project_name}";
 mysql -u${database_username} -p${database_password} -e "DROP DATABASE IF EXISTS ${full_database_name};" -vvv
 mysql -u${database_username} -p${database_password} -e "CREATE DATABASE ${full_database_name};" -vvv
 
-composer create-project vardot/vardoc-project:${site_version} ${project_name} --stability dev --no-interaction -vvv
+composer create-project vardot/vardoc-project:${site_version} ${project_name} --stability dev --no-interaction -vvv ;
 
-sudo chmod 775 -R ${project_name}
-sudo chown www-data:${user_name} -R ${project_name}
+
+cp ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/default.settings.php ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+echo "\$databases['default']['default'] = array (
+  'database' => '${full_database_name}',
+  'username' => '${database_username}',
+  'password' => '${database_password}',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);" >> ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+
+mkdir ${vdo_root}/${doc_name}/${project_name}/config ;
+mkdir ${vdo_root}/${doc_name}/${project_name}/config/sync ;
+echo "# Deny all requests from Apache 2.4+.
+<IfModule mod_authz_core.c>
+  Require all denied
+</IfModule>
+
+# Deny all requests from Apache 2.0-2.2.
+<IfModule !mod_authz_core.c>
+  Deny from all
+</IfModule>
+
+# Turn off all options we don't need.
+Options -Indexes -ExecCGI -Includes -MultiViews
+
+# Set the catch-all handler to prevent scripts from being executed.
+SetHandler Drupal_Security_Do_Not_Remove_See_SA_2006_006
+<Files *>
+  # Override the handler again if we're run later in the evaluation list.
+  SetHandler Drupal_Security_Do_Not_Remove_See_SA_2013_003
+</Files>
+
+# If we know how to do it safely, disable the PHP engine entirely.
+<IfModule mod_php7.c>
+  php_flag engine off
+</IfModule>" >> ${vdo_root}/${doc_name}/${project_name}/config/sync/.htaccess;
+echo "\$settings['config_sync_directory'] = '../config/sync';" >> ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+
+vdo_build_time=$( date '+%Y-%m-%d %H-%M-%S' );
+echo "// VDO Built time: ${vdo_build_time}" >> ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+
+sudo chmod 775 -R ${vdo_root}/${doc_name}/${project_name}
+sudo chown www-data:${user_name} -R ${vdo_root}/${doc_name}/${project_name}
 
 echo "${doc_name} ${project_name} is ready to install!!!!";
 echo "Go to ${base_url}";
