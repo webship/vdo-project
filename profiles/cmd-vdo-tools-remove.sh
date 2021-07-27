b4ca3c9d30f8d39e539d9dcbfaefd90b4274a001
Rajab Natshah <rajabn@gmail.com>
2021-07-27 11:44:52 +0300

    Add the cmd vdo tools remove command to be able to remove selected outdated projects #37

---
+++ profiles/cmd-vdo-tools-remove.sh
@@ -0,0 +1,30 @@
+#!/bin/usr/env bash
+
+# Bootstrap VDO.
+. ${vdo_scripts}/bootstrap.sh ;
+
+# Load workspace settings and extra lists.
+eval $(parse_yaml ${vdo_config}/workspace.profiles.settings.yml);
+
+
+# GET the project name argument.
+if [ "$1" != "" ]; then
+    project_name=$1;
+else
+  echo "Please add the name of your project.";
+  exit 1;
+fi
+
+
+# Change directory to the workspace for this full operation.
+cd ${vdo_root}/${doc_name};
+
+if [ -d "${project_name}" ]; then
+  echo "--------------";
+  sudo rm -rf ${project_name}
+  echo "Deleted: ${vdo_root}/${doc_name}/${project_name}";
+  echo "--------------";
+
+  full_database_name="${database_prefix}${project_name}";
+  mysql -u${database_username} -p${database_password} -e "DROP DATABASE IF EXISTS ${full_database_name};" -v
+fi
