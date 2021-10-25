6104b6a28b2a0558c05d76ae219bec1215231d41
RajabNatshah <rajabn@gmail.com>
2021-10-25 21:23:58 +0300

    Add cmd-vdo-uber_publisher7-project.sh for Uber Publisher 7.0.x-dev branch #39

---
+++ test/cmd-vdo-uber_publisher7-project.sh
@@ -0,0 +1,71 @@
+#!/bin/usr/env bash
+
+# Bootstrap VDO.
+. ${vdo_scripts}/bootstrap.sh ;
+
+# Load workspace settings and extra lists.
+eval $(parse_yaml ${vdo_config}/workspace.test.settings.yml);
+
+# Change with the version of Uber Publisher 7.0.x-dev
+site_version="7.0.x-dev";
+# Change with the version of Uber Publisher 70DEV
+site_version_code="70DEV";
+
+
+# Change to true if you want to install Uber Publisher.
+install_site=false;
+
+# GET the project name argument.
+if [ "$1" != "" ]; then
+    project_name=$1;
+else
+  echo "Please add the name of your project.";
+  exit 1;
+fi
+
+# GET install argument to install;
+if [ "$2" != "" ]; then
+  if [ "$2" == "install" ]; then
+    install_site=true;
+  fi
+fi
+
+# Change directory to the workspace for this full operation.
+cd ${vdo_root}/${doc_name};
+
+if [ -d "${project_name}" ]; then
+  sudo rm -rf ${project_name} -vvv
+fi
+
+full_database_name="${database_prefix}${project_name}";
+mysql -u${database_username} -p${database_password} -e "DROP DATABASE IF EXISTS ${full_database_name};" -vvv
+mysql -u${database_username} -p${database_password} -e "CREATE DATABASE ${full_database_name} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" -vvv
+
+composer create-project vardot/uber_publisher-project:${site_version} ${project_name} --stability dev --no-interaction -vvv ;
+
+cp ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/default.settings.php ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+echo "\$databases['default']['default'] = [
+  'database' => '${full_database_name}',
+  'username' => '${database_username}',
+  'password' => '${database_password}',
+  'host' => '${database_host}',
+  'port' => '${database_port}',
+  'namespace' => '${database_namespace}',
+  'driver' => '${database_driver}',
+  'prefix' => '',
+  'collation' => '${database_collation}',
+];" >> ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+
+mkdir ${vdo_root}/${doc_name}/${project_name}/config ;
+mkdir ${vdo_root}/${doc_name}/${project_name}/config/sync ;
+echo "\$settings['config_sync_directory'] = '${config_sync_directory}';" >> ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+
+vdo_build_time=$( date '+%Y-%m-%d %H-%M-%S' );
+echo "// VDO Built time: ${vdo_build_time}" >> ${vdo_root}/${doc_name}/${project_name}/docroot/sites/default/settings.php ;
+
+sudo chmod 775 -R ${vdo_root}/${doc_name}/${project_name}
+sudo chown www-data:${user_name} -R ${vdo_root}/${doc_name}/${project_name}
+
+echo "${doc_name} ${project_name} is ready to install!!!!";
+base_url="${web_url}/${project_name}/docroot";
+echo "Go to ${base_url}";
