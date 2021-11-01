1a137ed3e4c0cdca19351dac2ee834bb5cc35077
RajabNatshah <rajabn@gmail.com>
2021-11-01 14:03:24 +0200

    Add CMD VDO to get all repos for Uber Publisher cmd-vdo-uber_publisher-get-all-repos.sh #41

---
+++ products/uber-publisher/cmd-vdo-uber_publisher-get-all-repos.sh
@@ -0,0 +1,19 @@
+#!/bin/usr/env bash
+
+# Uber Publisher project template.
+git clone --branch '7.0.x' git@github.com:Vardot/uber_publisher-project.git
+# Uber Publisher profile.
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher.git
+
+# Uber Publisher components.
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_ads_system.git
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_author_profile.git
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_breaking_news.git
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_core.git
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_daily_cartoons.git
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_indepth.git
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_in_pictures.git
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_opinion_articles.git
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_search.git
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_social_post.git
+git clone --branch '7.0.x' git@git.drupal.org:project/uber_publisher_video.git
