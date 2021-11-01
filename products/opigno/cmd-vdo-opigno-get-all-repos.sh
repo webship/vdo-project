086a730b1a3885208f2304cc71f11eb2caf5bd9a
RajabNatshah <rajabn@gmail.com>
2021-11-01 14:02:41 +0200

    Add CMD VDO to get all repos for Opigno LMS cmd-vdo-opigno-get-all-repos.sh #42

---
+++ products/opigno/cmd-vdo-opigno-get-all-repos.sh
@@ -0,0 +1,31 @@
+#!/bin/usr/env bash
+
+# Opigno project template.
+git clone --branch '3.x' git@bitbucket.org:opigno/opigno-composer.git
+# Opigno LMS profile.
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_lms.git
+
+# Opigno components.
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_calendar.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_calendar_event.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_catalog.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_certificate.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_class.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_cron.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_course.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_dashboard.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_forum.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_group_manager.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_ilt.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_learning_path.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_messaging.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_module.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_moxtra.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_notification.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_scorm.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_search.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_statistics.git
+git clone --branch '3.x' https://git.drupalcode.org/project/opigno_tincan_api.git
+
+# Opigno themes.
+git clone --branch '3.x' https://git.drupalcode.org/project/aristotle.git
