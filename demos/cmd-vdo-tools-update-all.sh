8c11282752538f04e247db28ffdd905b4e09ad7f
RajabNatshah <rajabn@gmail.com>
2021-11-08 11:30:29 +0200

    Add CMD VDO tools update all command cmd-vdo-tools-update-all.sh #44

--- demos/cmd-vdo-tools-update-all.sh
+++ demos/cmd-vdo-tools-update-all.sh
@@ -1,15 +1,27 @@
+#!/bin/usr/env bash
+
 # Bootstrap VDO.
 . ${vdo_scripts}/bootstrap.sh ;
 
 # Load workspace settings and extra lists.
 eval $(parse_yaml ${vdo_config}/workspace.demos.settings.yml);
 
+cd ${vdo_root}/${doc_name};
+echo "=======================================================";
+echo "  Update all projects in ${vdo_root}/${doc_name} ";
+echo "=======================================================";
 for project in *; do
     if [ -d "$project" ]; then
-    echo "===========================================";
-    echo "  $project";
-    echo "===========================================";
-	cd ${vdo_root}/${doc_name}/${project};
-	composer update --no-interaction -vvv ;
-    fi
-done
+        echo "  $project   ";
+    fi;
+done;
+for project in *; do
+    if [ -d "$project" ]; then
+        echo "=======================================================";
+        echo "  ${vdo_root}/${doc_name}/${project}/   ";
+        echo "=======================================================";
+        cd ${vdo_root}/${doc_name}/${project};
+        composer update -v;
+    fi;
+    cd ${vdo_root}/${doc_name}
+done;
