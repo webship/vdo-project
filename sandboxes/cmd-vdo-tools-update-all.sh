f3f0d364de9574cd4f95b4439b1c57da50fb9026
RajabNatshah <rajabn@gmail.com>
2021-11-08 10:57:15 +0200

    Add CMD VDO tools update all command cmd-vdo-tools-update-all.sh #44

---
+++ sandboxes/cmd-vdo-tools-update-all.sh
@@ -0,0 +1,15 @@
+# Bootstrap VDO.
+. ${vdo_scripts}/bootstrap.sh ;
+
+# Load workspace settings and extra lists.
+eval $(parse_yaml ${vdo_config}/workspace.sandboxes.settings.yml);
+
+for project in *; do
+    if [ -d "$project" ]; then
+    echo "===========================================";
+    echo "  $project";
+    echo "===========================================";
+	cd ${vdo_root}/${doc_name}/${project};
+	composer update --no-interaction -vvv ;
+    fi
+done
