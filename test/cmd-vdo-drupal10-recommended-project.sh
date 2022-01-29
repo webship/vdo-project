6b4c67f3d8e9e53ce6692ab5deeb4b4fddf820f1
Rajab Natshah <rajabn@gmail.com>
2022-01-29 14:32:39 +0200

    Add VDO commands for Drupal 10 development and testing #56

--- test/cmd-vdo-drupal10-recommended-project.sh
+++ test/cmd-vdo-drupal10-recommended-project.sh
@@ -7,7 +7,7 @@ source ${vdo_scripts}/bootstrap.sh || exit 1 ;
 eval $(parse_yaml ${vdo_config}/workspace.test.settings.yml);
 
 # Set site version.
-site_version="~10";
+site_version="^10.0@alpha";
 
 # Load distribution configs.
 eval $(parse_yaml ${vdo_config}/distributions/drupal.yml);
