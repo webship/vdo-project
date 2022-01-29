752cf116655c4c0332ac05be65c2125898220d64
Rajab Natshah <rajabn@gmail.com>
2022-01-29 14:21:39 +0200

    Add VDO commands for Drupal 10 development and testing #56

---
+++ sandboxes/cmd-vdo-drupal10-recommended-project.sh
@@ -0,0 +1,21 @@
+#!/bin/usr/env bash
+
+# Bootstrap VDO.
+source ${vdo_scripts}/bootstrap.sh || exit 1 ;
+
+# Load workspace settings and extra lists.
+eval $(parse_yaml ${vdo_config}/workspace.sandboxes.settings.yml);
+
+# Set site version.
+site_version="~10";
+
+# Load distribution configs.
+eval $(parse_yaml ${vdo_config}/distributions/drupal.yml);
+
+ARGPARSE_DESCRIPTION="Build a ${distribution_title} ${site_version} project"
+source ${vdo_scripts}/args/arg-vdo-${distribution_name}.sh || exit 1 ;
+
+shift $#;
+
+# Build the distribution.
+build_distribution ;
