0df3f75cd36a644c03a044f04cb2b6551ac34de1
Rajab Natshah <rajabn@gmail.com>
2022-02-08 17:30:05 +0200

    Update the LAMP install commands to latest #57

--- vdo/scripts/install/setup-LAMP-ubuntu21-04--php8-00--MySQL8.sh
+++ vdo/scripts/install/setup-LAMP-ubuntu21-04--php8-00--MySQL8.sh
@@ -8,153 +8,129 @@ database_host="localhost";
 database_password="123___";
 
 # Usefull must have utils.
-apt install -y vim;
-apt install -y links;
-apt install -y lynx;
-apt install -y git;
-apt install -y diffutils;
-apt install -y htop;
-apt install -y curl;
-apt install -y wget;
-apt install -y p7zip-full;
-apt install -y unzip;
-apt install -y zip;
-apt install -y sed;
-apt install -y gcc;
-apt install -y g++;
-apt install -y make;
-apt install -y python;
-apt install -y python3;
+sudo apt install -y vim;
+sudo apt install -y links;
+sudo apt install -y lynx;
+sudo apt install -y git;
+sudo apt install -y diffutils;
+sudo apt install -y htop;
+sudo apt install -y curl;
+sudo apt install -y wget;
+sudo apt install -y p7zip-full;
+sudo apt install -y unzip;
+sudo apt install -y zip;
+sudo apt install -y sed;
+sudo apt install -y gcc;
+sudo apt install -y g++;
+sudo apt install -y make;
+sudo apt install -y python;
+sudo apt install -y python3;
 
 # PHP
-add-apt-repository ppa:ondrej/php ;
-apt update;
-apt install -y apache2;
-apt install -y php8.0;
-apt install -y libapache2-mod-php8.0;
-apt install -y php8.0-mbstring;
-apt install -y php8.0-mysql;
-apt install -y php8.0-xml;
-apt install -y php8.0-cgi;
-apt install -y php8.0-cli;
-apt install -y php8.0-common;
-apt install -y php8.0-curl;
-apt install -y php8.0-gd;
-apt install -y php8.0-imap;
-apt install -y php8.0-intl;
-apt install -y php8.0-pspell;
-apt install -y php8.0-sqlite3;
-apt install -y php8.0-tidy;
-apt install -y php8.0-opcache;
-apt install -y php8.0-json;
-apt install -y php8.0-bz2;
-apt install -y php8.0-readline;
-apt install -y php8.0-xmlrpc;
-apt install -y php8.0-enchant;
-apt install -y php8.0-xsl;
-apt install -y php-all-dev;
-apt install -y php8.0-zip;
-apt install -y php8.0-bcmath;
-apt install -y php-pear;
+sudo add-apt-repository ppa:ondrej/php ;
+sudo apt update;
+sudo apt install -y apache2;
+sudo apt install -y php8.0;
+sudo apt install -y libapache2-mod-php8.0;
+sudo apt install -y php8.0-mbstring;
+sudo apt install -y php8.0-mysql;
+sudo apt install -y php8.0-xml;
+sudo apt install -y php8.0-cgi;
+sudo apt install -y php8.0-cli;
+sudo apt install -y php8.0-common;
+sudo apt install -y php8.0-curl;
+sudo apt install -y php8.0-gd;
+sudo apt install -y php8.0-imap;
+sudo apt install -y php8.0-intl;
+sudo apt install -y php8.0-pspell;
+sudo apt install -y php8.0-sqlite3;
+sudo apt install -y php8.0-tidy;
+sudo apt install -y php8.0-opcache;
+sudo apt install -y php8.0-json;
+sudo apt install -y php8.0-bz2;
+sudo apt install -y php8.0-readline;
+sudo apt install -y php8.0-xmlrpc;
+sudo apt install -y php8.0-enchant;
+sudo apt install -y php8.0-xsl;
+sudo apt install -y php8.0-zip;
+sudo apt install -y php8.0-bcmath;
+sudo apt install -y php-pear;
 
 # Set the Apache2 enable mode to php8.0
-a2enmod php8.0 ;
-update-alternatives --set php /usr/bin/php8.0 ;
-service apache2 restart ;
+sudo a2enmod php8.0 ;
+sudo update-alternatives --set php /usr/bin/php8.0 ;
+sudo service apache2 restart ;
 
 # Make sure important apache modules are enabled.
-a2enmod headers rewrite env mime expires ssl;
+sudo a2enmod headers rewrite env mime expires ssl;
 
 # Change PHP variables.
-echo 'max_execution_time = 1200' >> /etc/php/8.0/apache2/php.ini ;
-echo 'max_input_time = 180' >> /etc/php/8.0/apache2/php.ini ;
-echo 'max_input_vars = 10000' >> /etc/php/8.0/apache2/php.ini ;
-echo 'memory_limit = -1' >> /etc/php/8.0/apache2/php.ini ;
-echo 'error_reporting = E_ALL' >> /etc/php/8.0/apache2/php.ini ;
-echo 'post_max_size = 64M' >> /etc/php/8.0/apache2/php.ini ;
-echo 'upload_max_filesize = 32M' >> /etc/php/8.0/apache2/php.ini ;
-echo 'max_file_uploads = 40' >> /etc/php/8.0/apache2/php.ini ;
+sudo echo 'max_execution_time = 1200' >> /etc/php/8.0/apache2/php.ini ;
+sudo echo 'max_input_time = 180' >> /etc/php/8.0/apache2/php.ini ;
+sudo echo 'max_input_vars = 10000' >> /etc/php/8.0/apache2/php.ini ;
+sudo echo 'memory_limit = -1' >> /etc/php/8.0/apache2/php.ini ;
+sudo echo 'error_reporting = E_ALL' >> /etc/php/8.0/apache2/php.ini ;
+sudo echo 'post_max_size = 64M' >> /etc/php/8.0/apache2/php.ini ;
+sudo echo 'upload_max_filesize = 32M' >> /etc/php/8.0/apache2/php.ini ;
+sudo echo 'max_file_uploads = 40' >> /etc/php/8.0/apache2/php.ini ;
 
-service apache2 restart ;
+sudo service apache2 restart ;
 
 
 # Set the virtual host.
-sed -i -e 's/<\/VirtualHost>/\t<Directory \/var\/www\/html >\n\t\tOptions Indexes FollowSymLinks MultiViews\n\t\tAllowOverride All\n\t\tOrder allow,deny\n\t\tallow from all\n\t\tRequire all granted\n\t<\/Directory>\n<\/VirtualHost>/g' /etc/apache2/sites-available/000-default.conf ;
+sudo sed -i -e 's/<\/VirtualHost>/\t<Directory \/var\/www\/html >\n\t\tOptions Indexes FollowSymLinks MultiViews\n\t\tAllowOverride All\n\t\tOrder allow,deny\n\t\tallow from all\n\t\tRequire all granted\n\t<\/Directory>\n<\/VirtualHost>/g' /etc/apache2/sites-available/000-default.conf ;
 
-service apache2 restart ;
+sudo service apache2 restart ;
 
 # Install node js;
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-apt install -y nodejs ;
-apt install -y build-essential ;
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt update
+sudo apt install -y nodejs ;
+sudo apt install -y build-essential ;
 
 # Install node npm.
 # ------------------------------------------------------------------------------
-curl -L https://npmjs.com/install.sh | sh ;
-
-# Install LESS + grunt.
-# ------------------------------------------------------------------------------
-apt install -y npm ;
-npm install -g less grunt grunt-cli ;
+curl -L https://npmjs.com/install.sh | sudo -E bash -
+sudo apt update
+sudo apt install npm
 
 # Install Yarn as a global by npm.
 # ------------------------------------------------------------------------------
-npm install -g yarn
+sudo npm install -g yarn
 
 # Install Gulp : Helps compiling SASS to CSS.
 # ------------------------------------------------------------------------------
-npm install gulp-cli -g ;
-npm install gulp -D ;
+sudo npm install gulp-cli -g ;
+sudo npm install gulp -D ;
 
 # Install composer ~2.0.
 sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin/ --filename=composer --2;
 
-# Drush install latest (10.x).
+# Drush install latest (11.x).
 # ------------------------------------------------------------------------------
-git clone https://github.com/drush-ops/drush.git /usr/local/src/drush ;
+sudo git clone https://github.com/drush-ops/drush.git /usr/local/src/drush ;
 cd /usr/local/src/drush ;
-git checkout 10.3.6 ;
-ln -s /usr/local/src/drush/drush /usr/bin/drush ;
-composer install ;
+git checkout 11.0.4 ;
+sudo ln -s /usr/local/src/drush/drush /usr/bin/drush ;
+sudo composer install ;
 
 # Install a global Drush via Composer.
-composer global require drush/drush:~10 ;
-export PATH="$HOME/.config/composer/vendor/bin:$PATH" ;
-
-# Install Drupal Console.
-# ------------------------------------------------------------------------------
-cd ${current_path} ;
-
-# Run this in your terminal to get the latest Console version:
-curl https://drupalconsole.com/installer -L -o drupal.phar ;
-
-# Or if you don't have curl:
-php -r "readfile('https://drupalconsole.com/installer');" > drupal.phar ;
-
-# Accessing from anywhere on your system:
-mv drupal.phar /usr/local/bin/drupal ;
-
-# Apply executable permissions on the downloaded file:
-chmod +x /usr/local/bin/drupal ;
-
-# Copy configuration files to user home directory.
-drupal init [--override] ;
-drupal self-update ;
-
+sudo composer global require drush/drush:~11.0 ;
+sudo export PATH="$HOME/.config/composer/vendor/bin:$PATH" ;
 
 # Install MySQL 8 server.
 # ------------------------------------------------------------------------------
-apt install mysql-client mysql-server ;
+sudo apt install mysql-client mysql-server ;
 
-echo "[mysqld]" >> /etc/mysql/my.cnf
-echo "default-authentication-plugin=mysql_native_password" >> /etc/mysql/my.cnf ;
-echo "max_allowed_packet = 32M" >> /etc/mysql/my.cnf ;
+sudo echo "[mysqld]" >> /etc/mysql/my.cnf
+sudo echo "default-authentication-plugin=mysql_native_password" >> /etc/mysql/my.cnf ;
+sudo echo "max_allowed_packet = 32M" >> /etc/mysql/my.cnf ;
 
 # Restart MySQL service.
-service mysql restart ;
+sudo service mysql restart ;
 
 # To change the password for root user.
 sudo mysql -u ${database_username} -e "use mysql;SET PASSWORD FOR '${database_username}'@'${database_host}' = '${database_password}';update user set plugin=\"mysql_native_password\" where User='${database_username}';flush privileges;quit" ;
 
 # Restart MySQL service.
-service mysql restart ;
+sudo service mysql restart ;
