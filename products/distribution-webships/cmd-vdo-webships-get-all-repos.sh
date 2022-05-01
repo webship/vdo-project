fc0dc3ab81f8ab1015ae22dc5ab2745ac0723e68
Rajab Natshah <rajabn@gmail.com>
2022-05-01 12:36:22 +0300

    Change webships/webships-project to webship/webships-project #61

--- products/distribution-webships/cmd-vdo-webships-get-all-repos.sh
+++ products/distribution-webships/cmd-vdo-webships-get-all-repos.sh
@@ -1,6 +1,6 @@
 #!/bin/usr/env bash
 
 # Webships project template.
-git clone --branch '9.0.x' git@github.com:webships/webships-project.git
+git clone --branch '9.0.x' git@github.com:webship/webships-project.git
 # Webships profile.
 git clone --branch '9.0.x' git@git.drupal.org:project/webships.git
