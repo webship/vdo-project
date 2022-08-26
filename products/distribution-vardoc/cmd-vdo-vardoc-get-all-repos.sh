7b7fa162f8fd155eafe612efd6709449702f341c
Rajab Natshah <rajabn@gmail.com>
2022-08-27 01:02:59 +0300

    Change the distribution vardoc get all repos for Vardoc project template and Vardoc profile to the 5.0.x branch #71

--- products/distribution-vardoc/cmd-vdo-vardoc-get-all-repos.sh
+++ products/distribution-vardoc/cmd-vdo-vardoc-get-all-repos.sh
@@ -1,6 +1,6 @@
 #!/bin/usr/env bash
 
 # Vardoc project template.
-git clone --branch '9.0.x' git@github.com:vardot/vardoc-project.git
+git clone --branch '5.0.x' git@github.com:vardot/vardoc-project.git
 # Vardoc profile.
-git clone --branch '9.0.x' git@git.drupal.org:project/vardoc.git
+git clone --branch '5.0.x' git@git.drupal.org:project/vardoc.git
