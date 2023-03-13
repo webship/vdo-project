76374761bd31f9c74da65bf39aca329b647a762b
Rajab Natshah <rajabn@gmail.com>
2023-03-13 14:25:26 +0300

    Add Varbase Gleap module and add VDO commands for Gleap #82

--- vdo/scripts/functions/fun-vdo-gleap.sh
+++ vdo/scripts/functions/fun-vdo-gleap.sh
@@ -6,3 +6,21 @@ function add_gleap() {
   mkdir -p ${vdo_root}/${doc_name}/${PROJECT_NAME}/${distribution_webroot}/modules/custom ;
   cp -r ${vdo_assets}/drupal/modules/varbase_gleap ${vdo_root}/${doc_name}/${PROJECT_NAME}/${distribution_webroot}/modules/custom/
 }
+
+# Enable Gleap.
+function enable_gleap() {
+  # Change directory to the project.
+  cd ${vdo_root}/${doc_name}/${PROJECT_NAME}/${distribution_webroot} ; 
+
+  echo "Enable Gleap";
+  ../vendor/drush/drush/drush pm:enable varbase_gleap
+}
+
+# Disable Gleap.
+function disable_gleap() {
+  # Change directory to the project.
+  cd ${vdo_root}/${doc_name}/${PROJECT_NAME}/${distribution_webroot} ;
+
+  echo "Disable Gleap";
+  ../vendor/drush/drush/drush pm:uninstall varbase_gleap
+}
