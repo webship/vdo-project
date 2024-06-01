f8f2309ad49e8cade18c8ad9f49b9569d22c2bed
Rajab Natshah <rajabn@gmail.com>
2024-06-01 15:17:09 +0300

    Move all development workflow to Drupal ~10.3.0 and Drupal ~11.0 #97

---
+++ dev/cmd-vdo-webships2-0-x-project.sh
@@ -0,0 +1,21 @@
+#!/bin/usr/env bash
+
+# Bootstrap VDO.
+source ${vdo_scripts}/bootstrap.sh || exit 1 ;
+
+# Load workspace settings and extra lists.
+eval $(parse_yaml ${vdo_config}/workspace.dev.settings.yml);
+
+# Set the version.
+site_version="2.0.x-dev";
+
+# Load distribution configs.
+eval $(parse_yaml ${vdo_config}/distributions/webships.yml);
+
+ARGPARSE_DESCRIPTION="Build a ${distribution_title} ${site_version} project"
+source ${vdo_scripts}/args/arg-vdo-${distribution_name}.sh || exit 1 ;
+
+shift $#;
+
+# Build the distribution.
+build_distribution ;
--- products/distribution-webship/cmd-vdo-webship-get-all-repos.sh
+++ products/distribution-webship/cmd-vdo-webship-get-all-repos.sh
@@ -1,6 +1,6 @@
 #!/bin/usr/env bash
 
 # Webship project template.
-git clone --branch '9.0.x' git@github.com:webship/webship-project.git
+git clone --branch '10.0.x' git@github.com:webship/webship-project.git
 # Webship profile.
-git clone --branch '9.0.x' git@git.drupal.org:project/webship.git
+git clone --branch '10.0.x' git@git.drupal.org:project/webship.git
